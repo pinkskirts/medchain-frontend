3d87264c2aafa8204784179ee57541b5f7399a46
scagl1 <gugascaglione2@gmail.com>
2024-02-04 06:28:11 -0300

    build: add prescriptionLoaderHandler + useEffect + db data mapping

--- frontend/src/components/dashboard/read-prescriptions.js
+++ frontend/src/components/dashboard/read-prescriptions.js
@@ -3,11 +3,37 @@ import { useEffect, useState } from "react";
 function ReadPrescriptions() {
   const [data, setData] = useState([]);
 
+  const loadPrescriptionsHandler = async () => {
+    await fetch("api/prescription", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.payload);
+      });
+  };
+
+  useEffect(() => { //todo: fix component props to show results dinamically
+    loadPrescriptionsHandler();
+  }, []);
+
   return (
     <div>
       <section>
         <h1>Available Prescriptions</h1>
         <ul>
+          {data.map((item) => (
+            <li key={item.ID}>
+              ID: {item.ID} <br />
+              CRM: {item.CRM} <br />
+              Patient: {item.patient} <br />
+              Medicine: {item.medicine} <br />
+              DOI: {item.date} <br />
+            </li>
+          ))}
         </ul>
       </section>
     </div>
