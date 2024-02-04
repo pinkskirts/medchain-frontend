0255a95623ed7b305023d97bc63c623c0e65f26b
scagl1 <gugascaglione2@gmail.com>
2024-02-04 06:24:08 -0300

    build: add create and delete prescription method

--- frontend/src/components/dashboard/write-prescriptions-form.js
+++ frontend/src/components/dashboard/write-prescriptions-form.js
@@ -1,4 +1,49 @@
 import { useRef, useState } from "react";
+
+async function createPrescription(CRM, patientName, medicineName) {
+  const integerCRM = parseInt(CRM);
+
+  const response = await fetch("api/prescription", {
+    method: "POST",
+    body: JSON.stringify({
+      integerCRM,
+      patientName,
+      medicineName,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not create the prescription!");
+  }
+
+  return data;
+}
+
+async function deletePrescription(prescriptionID) {
+  const response = await fetch("api/prescription", {
+    method: "DELETE",
+    body: JSON.stringify({
+      prescriptionID,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not delete the prescription!");
+  }
+
+  return data;
+}
+
 function WritePrescriptionsForms() {
   const createFormRef = useRef();
 
@@ -9,6 +54,27 @@ function WritePrescriptionsForms() {
   const [patientName, setPatientName] = useState("");
   const [medicineName, setMedicineName] = useState("");
   const [prescriptionID, setPrescriptionID] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (event.target === createFormRef.current) {
+        // CREATE/POST
+        await createPrescription(CRM, patientName, medicineName);
+        alert("Prescription created successfully!");
+      } else {
+        // DELETE
+        await deletePrescription(prescriptionID);
+        alert("Prescription deleted successfully!");
+      }
+      const form = event.target;
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <section>
