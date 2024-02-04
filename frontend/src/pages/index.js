2b50184d0539c07fedf2d4462e2804a0d20dcfca
scagl1 <gugascaglione2@gmail.com>
2024-02-04 05:50:54 -0300

    build: add main-header component props + add additional headings to / route

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,8 +3,9 @@ import MainHeader from "@/components/main-header/main-header";
 export default function HomePage() {
   return (
     <>
-      <MainHeader />
-      <h1>MedChain Home</h1>
+      <MainHeader showLogin showDashboard/>
+      <h1>MedChain HomePage</h1>
+      <h3>We provide the best way of storing medical prescriptions: Blockchain tecnology!</h3>
     </>
   );
 }
