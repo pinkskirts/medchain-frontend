17d05bd0ef8c38aea6f770db0afe891e973a8f57
Gustavo Scaglione <gugascaglione2@gmail.com>
2024-03-20 09:42:52 -0300

    build: add main file for db connection - module imports - connection handler

---
+++ backend/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:                 "root",
+		Passwd:               "",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "medchain",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+
+	// Database handle
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if pingErr := db.Ping(); pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Print("Sucessfully connected to DB!\n")
+
+}
+
