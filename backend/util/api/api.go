acc0db70a1a7e84b08e806ab47182b3426af1241
Gustavo Scaglione <gugascaglione2@gmail.com>
2024-03-23 17:14:01 -0300

    build: set /users route in api package

--- backend/util/api/api.go
+++ backend/util/api/api.go
@@ -1,9 +1,31 @@
 package api
 
 import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/julienschmidt/httprouter"
+	"medchainbackend/util/crud"
 )
 
-func test() {
-	
+const port string = ":8080"
+
+func ServerInit() {
+	router := httprouter.New()
+
+	// Static route config
+	router.GET("/users/", getUser)
+
+	// Start HTTP server
+	http.ListenAndServe(port, router)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	err := crud.ReadPrescriptions()
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(err) // todo: null object
 }
