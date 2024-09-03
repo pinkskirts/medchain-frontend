fc7c4d7206b1f4f897ead39cf4e238b8c88abd58
scaglibcr <gustavo.scaglione@bcrcx.com>
2024-09-02 23:05:05 -0300

    feature: adiciona pagina ForgotPassword

---
+++ src/pages/Login/ForgotPassword/index.tsx
@@ -0,0 +1,9 @@
+import * as _ from "./styles";
+
+export default function ForgotPassword() {
+  return (
+    <_.Container>
+      Esqueci minha senha - pagina
+    </_.Container>
+  );
+}
---
+++ src/pages/Login/ForgotPassword/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-wrap: wrap;
+
+  background-image: url(); // placeholder
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+`;
